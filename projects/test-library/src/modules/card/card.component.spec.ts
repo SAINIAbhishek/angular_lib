9bd3737eb0a1e5fb053fcbd9bc868c76ed72d082
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-01-16 13:34:53 +0100

    publish library with angular command.

--- projects/test-library/src/modules/card/card.component.spec.ts
+++ projects/test-library/src/modules/card/card.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestLibraryComponent } from './test-library.component';
+import { CardComponent } from './card.component';
 
-describe('TestLibraryComponent', () => {
-  let component: TestLibraryComponent;
-  let fixture: ComponentFixture<TestLibraryComponent>;
+describe('CardComponent', () => {
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestLibraryComponent ]
+      declarations: [ CardComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TestLibraryComponent);
+    fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- projects/test-library/src/public-api.ts
+++ projects/test-library/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of test-library
  */
 
-export * from './lib/test-library.service';
-export * from './lib/test-library.component';
-export * from './lib/test-library.module';
+export * from './modules/modal/public-api';
+export * from './modules/card/public-api';
