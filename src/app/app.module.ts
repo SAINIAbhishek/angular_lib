1baf85fd7e38baba006ade1de243a22c8f8a5935
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-01-16 12:48:07 +0100

    disable ivy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ModalModule} from "@test/test-modal";
+import {CardModule, ModalModule} from "@test/test-modal";
+
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {ModalModule} from "@test/test-modal";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ModalModule
+    CardModule,
+    ModalModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
